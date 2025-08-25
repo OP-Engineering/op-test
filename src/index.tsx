e5dbded74562b11f22dbe13aa4d32ac8ca188f56
Oscar Franco <ospfranco@gmail.com>
2025-08-25 22:20:04 +0200

    Fix safeareacontext

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type TestFunction = () => void | Promise<void>;
 type HookFunction = () => void | Promise<void>;
